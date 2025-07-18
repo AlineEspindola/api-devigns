86da946f4a7dd366761a0ef075a1273cb6dda5e0
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2025-07-18 17:10:54 -0300

    feat: Adiciona tabela chapter

--- config/db.js
+++ config/db.js
@@ -21,34 +21,46 @@ function initDatabase() {
       }
       console.log("Usando banco de dados: devigns");
 
-      const createTable1 = `
-        CREATE TABLE IF NOT EXISTS turn (
-          turn_ID INT AUTO_INCREMENT PRIMARY KEY,
-          status ENUM('DONE', 'IN_PROGRESS') NOT NULL DEFAULT 'IN_PROGRESS',
-          sanity TINYINT NOT NULL DEFAULT 50,
-          knowledge TINYINT NOT NULL DEFAULT 50,
-          money TINYINT NOT NULL DEFAULT 50
-        );
-      `;
+      const createTables = [
+        {
+          name: "turn",
+          sql: `
+            CREATE TABLE IF NOT EXISTS turn (
+              turn_ID INT AUTO_INCREMENT PRIMARY KEY,
+              status ENUM('DONE', 'IN_PROGRESS') NOT NULL DEFAULT 'IN_PROGRESS',
+              sanity TINYINT NOT NULL DEFAULT 50,
+              knowledge TINYINT NOT NULL DEFAULT 50,
+              money TINYINT NOT NULL DEFAULT 50
+            );
+          `,
+        },
+        {
+          name: "chapter",
+          sql: `
+            CREATE TABLE IF NOT EXISTS chapter (
+              chapter_ID INT AUTO_INCREMENT PRIMARY KEY,
+              text TEXT NOT NULL,
+              turn_ID INT,
+              CONSTRAINT fk_turn
+                FOREIGN KEY (turn_ID) REFERENCES turn(turn_ID)
+                ON DELETE SET NULL
+                ON UPDATE CASCADE
+            );
+          `,
+        },
+      ];
 
-      // const createTable2 = `
-      //   CREATE TABLE IF NOT EXISTS chapter (
-      //     chapter_ID INT AUTO_INCREMENT PRIMARY KEY,
-      //     text TEXT NOT NULL,
-      //     turn_ID INT,
-      //     CONSTRAINT fk_turn
-      //       FOREIGN KEY (turn_ID) REFERENCES turn(id)
-      //       ON DELETE SET NULL
-      //       ON UPDATE CASCADE
-      //     );
-      // `;
-
-      db.query(createTable1, (err) => {
-        if (err) {
-          console.error('Erro ao criar tabela "turn" :', err.message);
-          return;
-        }
-        console.log('Tabela "turn" verificada/criada');
+      createTables.forEach((table) => {
+        db.query(table.sql, (err) => {
+          if (err) {
+            console.error(
+              `Erro ao criar tabela "${table.name}" :`,
+              err.message
+            );
+          } else {
+            console.log(`Tabela "${table.name}" verificada/criada`);
+          }
+        });
       });
     });
   });
